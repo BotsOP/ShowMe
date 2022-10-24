f882cda5557997a5e7f6912f426761b4880e72ed
Denis Dvorkovyy <prutmaild@gmail.com>
2022-10-24 11:11:12 +0200

    test

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"hello world");
     }
 
     // Update is called once per frame
